8f7b84315e11020010cca9d42677ae0b186ba578
Henriquelay <37563861+Henriquelay@users.noreply.github.com>
2025-07-25 05:23:51 -0300

    fancy git

--- hosts/acad-router/henriquelay/programs/git.nix
+++ hosts/acad-router/henriquelay/programs/git.nix
@@ -1,28 +1,36 @@
 { config, pkgs, ... }:
 {
-  programs.git = {
-    enable = true;
-    userName = "Henriquelay";
-    userEmail = "37563861+Henriquelay@users.noreply.github.com";
-    signing = {
-      key = "B3903EAC57AD1331995CD96202843DDA217C9D81";
-      signByDefault = true;
+  programs = {
+    git = {
+      enable = true;
+      userName = "Henriquelay";
+      userEmail = "37563861+Henriquelay@users.noreply.github.com";
+      delta = {
+        enable = true;
+        hyperlinks = true;
+      };
+      signing = {
+        key = "B3903EAC57AD1331995CD96202843DDA217C9D81";
+        signByDefault = true;
+      };
+      aliases = {
+        sw = "switch";
+        br = "branch";
+        co = "checkout";
+        st = "status";
+        hs = "log --pretty='%C(yellow)%h %Cblue%aN %C(cyan)%cd%C(auto)%d %Creset%s' --graph --date=relative --date-order";
+      };
+      extraConfig = {
+        init.defaultBranch = "main";
+        push.autoSetupRemote = true;
+        pull.rebase = true;
+      };
     };
-    aliases = {
-      sw = "switch";
-      br = "branch";
-      co = "checkout";
-      st = "status";
-      hs = "log --pretty='%C(yellow)%h %Cblue%aN %C(cyan)%cd%C(auto)%d %Creset%s' --graph --date=relative --date-order";
-    };
-    extraConfig = {
-      init.defaultBranch = "main";
-      push.autoSetupRemote = true;
-      pull.rebase = true;
-    };
-  };
 
-  programs.lazygit = {
-    enable = false;
+    lazygit.enable = false;
+
+    gitui = {
+      enable = true;
+    };
   };
 }
